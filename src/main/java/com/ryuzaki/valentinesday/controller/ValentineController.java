da5fc7fd6d745e9ab31428ebeb7b65476bb4459e
NotRyuzaki <kotliar.artem@gmail.com>
2026-02-08 17:02:50 -0500

    final fix

--- src/main/java/com/ryuzaki/valentinesday/controller/ValentineController.java
+++ src/main/java/com/ryuzaki/valentinesday/controller/ValentineController.java
@@ -2,7 +2,6 @@ package com.ryuzaki.valentinesday.controller;
 
 import com.ryuzaki.valentinesday.service.ValentineService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
